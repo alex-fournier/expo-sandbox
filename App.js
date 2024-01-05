bde6f0c59a18b975e25e19d11bfe4a29fced7cb0
Alex Fournier <alexandre.fournier@lyra-network.com>
2024-01-05 10:26:34 +0100

    add reproductible demo

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import { Image } from 'expo-image'
+import IconLogin from './assets/login.svg'
+import IconSnow from './assets/snow.svg'
+import IconReact from './assets/react.svg'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app !</Text>
+      <View style={styles.iconContainer}>
+        <Image source={IconLogin} style={styles.icon} tintColor="coral" />
+        <Image source={IconSnow} style={styles.icon} tintColor="coral" />
+        <Image source={IconReact} style={styles.icon} tintColor="coral" />
+      </View>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'grey',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  iconContainer: {
+    flexDirection: 'row',
+    marginVertical: 20,
+  },
+  icon: {
+    width: 60,
+    height: 60,
+    tintColor: 'coral',
+  },
+})
