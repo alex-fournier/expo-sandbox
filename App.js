5937a893797a3159b5b6c7e2a928b99e0bca6145
Alex Fournier <alexandre.fournier@lyra-network.com>
2024-02-22 16:22:31 +0100

    add expo-device repro

--- App.js
+++ App.js
@@ -1,19 +1,12 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
-import { Image } from 'expo-image'
-import IconLogin from './assets/login.svg'
-import IconSnow from './assets/snow.svg'
-import IconReact from './assets/react.svg'
+import * as Device from 'expo-device'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app !</Text>
-      <View style={styles.iconContainer}>
-        <Image source={IconLogin} style={styles.icon} tintColor="coral" />
-        <Image source={IconSnow} style={styles.icon} tintColor="coral" />
-        <Image source={IconReact} style={styles.icon} tintColor="coral" />
-      </View>
+      <Text style={styles.bold}>Device.designName = {Device.designName}</Text>
+      <Text style={styles.bold}>Device.productName = {Device.productName}</Text>
       <StatusBar style="auto" />
     </View>
   )
@@ -22,17 +15,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'grey',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  iconContainer: {
-    flexDirection: 'row',
-    marginVertical: 20,
-  },
-  icon: {
-    width: 60,
-    height: 60,
-    tintColor: 'coral',
+  bold: {
+    fontWeight: 'bold',
   },
 })
